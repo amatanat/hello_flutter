2c778a1ecf9a5bfd08d269964ed83153c34dd9a4
amatanat <ahmadova.matanat01@gmail.com>
2018-06-06 18:06:23 +0200

    Start building Flutter app. Use MaterialApp, Scaffold widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,29 @@
+// use this material package to make changes in UI of a Flutter app.
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Flutter'),
-        ),
-        body: HelloFlutter(),
-      ),
-    ),
+// top level function
+Widget helloFlutter(){
+  return Container(
+      color: Colors.green
   );
 }
 
-class HelloFlutter extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello Flutter!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
+void main() {
+  // UI of an app
+  runApp(
+    // MaterialApp is a widget in a Flutter that contains
+      // title, home, theme,
+    MaterialApp(
+      // remove the slow mode banner in the top right corner of an app
+      debugShowCheckedModeBanner: false,
+      // scaffold is another widget that offers drawers, app bars,
+        // bottom nav, fabs, and tabs.
+      home:  Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Flutter'),
         ),
-      ),
-    );
-  }
+        body: helloFlutter()
+      )
+    )
+  );
 }
